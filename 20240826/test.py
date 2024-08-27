6e43e9dbdaf8b37ed257dfd6e5471a07ef4f1e8d
jianlongIT <328852120@qq.com>
2024-08-28 00:25:18 +0800

    28commit

--- 20240826/test.py
+++ 20240826/test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Auther : jianlong
-import os
+import os, hashlib
 from glob import glob
 
 data = {}
@@ -16,19 +16,21 @@ def clear(path):
             is_delete = True
             with open(p, 'r')as f:
                 content = f.read()
+                hash_content_obj = hashlib.md5(content.encode('utf-8'))
+                hash_content = hash_content_obj.hexdigest()
             if file_name not in data:
                 data[file_name] = {
-                    p: content
+                    p: hash_content
                 }
             else:
                 for k, v in data[file_name].items():
-                    if v == content:
+                    if v == hash_content:
                         is_delete = True
                         os.remove(p)
                     else:
                         is_delete = False
                 if not is_delete:
-                    data[file_name][p] = content
+                    data[file_name][p] = hash_content
 
 
 if __name__ == '__main__':
